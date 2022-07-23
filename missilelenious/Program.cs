372cbbddf0acb56195ada78e377123702939cc90
BiniamLuel <biniamgebremedhin5@gmail.com>
2022-07-22 23:07:46 -0400

    practice added

---
+++ missilelenious/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace missilelenious
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool equal = Calculator<int>.AreEqual(4, 4);
+            bool strEqual = Calculator<string>.AreEqual("Interview", "Happy");
+
+
+
+            Console.WriteLine(equal + " " + strEqual);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Calculator<T> 
+    {
+        public static bool AreEqual (T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
